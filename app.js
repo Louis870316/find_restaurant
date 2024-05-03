127856cbd9394991a559e93501de7325c08f25a2
Louis870316 <lsd870316@gmail.com>
2024-05-03 21:47:39 +0800

    add mongoose

--- app.js
+++ app.js
@@ -1,6 +1,25 @@
 const express = require('express')
+const mongoose = require('mongoose')
+
+// 加入這段 code, 僅在非正式環境時, 使用 dotenv
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
 const app = express()
 const port = 3000
+
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+
+const db = mongoose.connection
+
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
 
 const exphbs = require('express-handlebars')
 const restaurantsData = require('./restaurant.json').results
@@ -33,7 +52,7 @@ app.get('/restaurants/:id', (req, res) => {
 })
 
 app.get('/', (req, res) => {
-  
+
 })
 
 app.listen(port, () => {
