d098092d66f51403d7ff2331086acea6652da41a
Louis870316 <lsd870316@gmail.com>
2024-05-09 13:03:46 +0800

    Adjustment

--- routes/index.js
+++ routes/index.js
@@ -3,10 +3,8 @@ const router = express.Router()
 
 const home = require('./modules/home')
 const restaurant = require('./modules/restaurant')
-const search = require('./modules/search')
 
 router.use('/', home)
 router.use('/restaurants', restaurant)
-router.use('/', search)
 
 module.exports = router
